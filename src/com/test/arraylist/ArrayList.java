c781395c30136f37edd78fdb65356a81c0c5ce81
程志鹏 <29175154652@qq.com>
2021-12-15 19:55:55 +0800

    test初始化

---
+++ src/com/test/arraylist/ArrayList.java
@@ -0,0 +1,41 @@
+package com.test.arraylist;
+
+public class ArrayList {
+    private static final char A = 1;
+
+    public static void main(String[] args) {
+//       char a=A;
+//       char b=0;
+//       char c=0;
+//       char d=0;
+//       char e=0;
+//       char f=0;
+//       char g=0;
+//       char h=0;
+//       char i=0;
+//       char j=0;
+//       char k=0;
+//       char l=0;
+//       char m=0;
+//       char n=0;
+//       char o=0;
+//       char p=0;
+//       char q=0;
+//       char r=0;
+//       char s=0;
+//       char t=0;
+//       char u=0;
+//       char v=0;
+//       char w=0;
+//       char x=0;
+//       char y=0;
+//       char z=0;
+//       char[] arr=new char[]{a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z};
+//
+//        System.out.println(arr);
+
+        }
+    }
+
+
+
---
+++ src/com/test/arraylist/Name.java
@@ -0,0 +1,19 @@
+package com.test.arraylist;
+
+import java.sql.SQLOutput;
+
+public class Name {
+   public static void main(String[] args) {
+      String a= "张三";
+      String b= "李四";
+      String c= "王二";
+      String d= "麻子";
+      String[]arr=new String[]{a,b,c,d} ;
+      for(int i=0;i<arr.length;i++){
+         System.out.println(arr[i]);
+      }
+
+
+   }
+
+}
